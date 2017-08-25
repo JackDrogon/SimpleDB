f1ce8e9df8aa3b78c56d0ca1fab84aabcfe6250b
Jack Drogon <jack.xsuperman@gmail.com>
2017-08-25 10:19:38 +0800

    Add db_full_lru_test

---
+++ test/db_full_lru_test.cc
@@ -0,0 +1,28 @@
+#include <string>
+#include <iostream>
+
+#include "db.h"
+
+using namespace std;
+
+int main()
+{
+	DB db("hello");
+
+	std::string key("hello");
+	std::string value("world");
+
+	for (int i = 0; i < 2000; ++i) {
+		db.Put(key+to_string(i), value+to_string(i));
+	}
+
+
+	std::string get_value;
+	if (db.Get(key+"1", get_value)) {
+		cout << key << " => " << get_value << endl;
+	} else {
+		cout << "Not found" << endl;
+	}
+
+	return 0;
+}
